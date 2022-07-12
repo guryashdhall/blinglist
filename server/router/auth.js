04a0cd2806daecbbef53aa8c83e287c1840a1456
Guryash Singh Dhall <gr622265@dal.ca>
2022-07-12 17:49:48 -0300

    Added edit profile, reset password API's

--- server/router/auth.js
+++ server/router/auth.js
@@ -3,6 +3,9 @@ const { User } = require("../models/User");
 
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
 
 //SIGNUP API
 router.post('/signup', (req, res) => {
@@ -131,35 +134,162 @@ router.post('/login', (req, res) => {
             });
 
 
+// FORGOT PASSWORD API
 
-
-
-
-
-
-
-
-
-
-        router.post('/forgot-password', (req, res) => {
-            const { email, securityQuestion, securityAnswer } = req.body;
-            if (!email | !securityQuestion | !securityAnswer) {
-                res.status(422).send('Please fill out all fields');
+router.post('/forgot-password', (req, res) => {
+    const { email, securityQuestion, securityAnswer } = req.body;
+    if (!email | !securityQuestion | !securityAnswer) {
+        res.status(422).send('Please fill out all fields');
+    }
+    else {
+        User.findOne({ email: email })
+        .then(savedUser => {
+            if (!savedUser) {
+                return res.status(422).json({
+                    success: false,
+                    message: 'User does not exist'
+                });
             }
             else {
-                res.status(200).send('Password reset email sent');
-            }
+                if (savedUser.securityQuestion !== securityQuestion || savedUser.securityAnswer !== securityAnswer) {
+                    return res.status(422).json({
+                        success: false,
+                        message: 'Incorrect security question or answer'
+                    });
+                }
+                else {
+                    return res.status(200).json({
+                        success: true,
+                        message: 'Credentials verified successfully',
+                        user: savedUser
+                    })
+                }
+        }})
+        .catch (err => {
+                return res.status(500).json({
+                    success: false,
+                    message: 'Error logging in',
+                    error: err
+                })
+            })
         }
-        );
-        router.post('/reset-password', (req, res) => {
-            const { password, confirmPassword } = req.body;
-            if (!password | !confirmPassword) {
-                res.status(422).send('Please fill out all fields');
-            }
-            else {
-                res.status(200).send('Password reset');
-            }
+    }
+);
+
+
+
+
+
+//RESET PASSWORD API
+
+router.post('/reset-password', (req, res) => {
+    const { email, password, confirmPassword } = req.body;
+    if (!email | !password | !confirmPassword) {
+        return res.status(422).json({
+            success: false,
+            message: 'Please fill out all fields'
         });
+    }
+    else if (password !== confirmPassword) {
+        return res.status(422).json({
+            success: false,
+            message: 'Passwords do not match'
+        });
+    }
+    else {
+        User.findOne({ email: email })
+            .then(savedUser => {
+                if (!savedUser) {
+                    return res.status(422).json({
+                        success: false,
+                        message: 'User does not exist'
+                    });
+                }
+                else {
+                    bcrypt.hash(password, 12)
+                        .then(hashedPassword => {
+                            savedUser.password = hashedPassword;
+                            savedUser.confirmPassword = hashedPassword;
+                            savedUser.save()
+                                .then(user => {
+                                    return res.status(200).json({
+                                        success: true,
+                                        message: 'Password reset successfully',
+                                        user: user
+                                    })
+                                })
+                                .catch(err => {
+                                    return res.status(500).json({
+                                        success: false,
+                                        message: 'Error resetting password',
+                                        error: err
+                                    })
+                                })
+                        })
+                }
+            }).catch(err => {
+                return res.status(500).json({
+                    success: false,
+                    message: 'Error resetting password',
+                    error: err
+                })
+            })        
+    }
+});
 
 
-        module.exports = router;
+// EDIT PROFILE API
+
+router.post('/edit-profile', (req, res) => {
+    const { firstName, lastName, email} = req.body;
+    if (!firstName | !lastName | !email) {
+        return res.status(422).json({
+            success: false,
+            message: 'Please fill out all fields'
+        })
+    }
+    else {
+        User.findOne({ email: email })
+            .then(savedUser => {
+                if (!savedUser) {
+                    return res.status(422).json({
+                        success: false,
+                        message: 'User does not exist'
+                    })
+                }
+                else {
+                    savedUser.firstName = firstName;
+                    savedUser.lastName = lastName;
+                    savedUser.email = email;
+                    savedUser.save()
+                        .then(user => {
+                            return res.status(200).json({
+                                success: true,
+                                message: 'Profile updated successfully',
+                                user: user
+                            })
+                        })
+                        .catch(err => {
+                            return res.status(500).json({
+                                success: false,
+                                message: 'Error updating profile',
+                                error: err
+                            })
+                        })
+                }
+
+            }).catch(err => {
+
+                return res.status(500).json({
+                    success: false,
+                    message: 'Error updating profile',
+                    error: err
+                })      
+            }
+            )
+    }
+}
+);
+
+
+module.exports = router;
