2064126415e87685cfbd2e71234def9377418f1b
AayushiGandhi <ay753882@dal.ca>
2022-07-13 22:32:06 -0300

    display products based on categories

--- server/router/admin.js
+++ server/router/admin.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const { getProducts, getProduct, createProduct, deleteProduct, editProduct } = require("../controllers/adminController");
 
-router.get("/admin", getProducts);
+router.get("/admin/:search", getProducts);
 router.get("/admin/:id", getProduct);
 router.post("/admin", createProduct);
 router.delete("/admin/:id", deleteProduct)
